5dcc6ad761bd77e08c66c1c3cfab736248781f08
Eric Barb <eric.barb@hotmail.com>
2021-04-04 14:17:34 -0400

    Banner is still fucked, but not causing errors.

--- scripts/10-banner.sh
+++ scripts/10-banner.sh
@@ -4,7 +4,7 @@ touch /etc/ssh/sshd-banner
 
 echo "     _______.___________.____    ____  __       ________   ________" > /etc/ssh/sshd-banner
 echo "    /       |           |\   \  /   / |  |     |       /  |       /" > /etc/ssh/sshd-banner
-echo "   |   (----`---|  |----` \   \/   /  |  |     `---/  /   `---/  /" > /etc/ssh/sshd-banner
+echo "   |   (----\`---|  |----\` \   \/   /  |  |     \`---/  /   \`---/  /" > /etc/ssh/sshd-banner
 echo "    \   \       |  |       \_    _/   |  |        /  /       /  /" > /etc/ssh/sshd-banner
-echo ".----)   |      |  |         |  |     |  `----.  /  /----.  /  /----." > /etc/ssh/sshd-banner
+echo ".----)   |      |  |         |  |     |  \`----.  /  /----.  /  /----." > /etc/ssh/sshd-banner
 echo "|_______/       |__|         |__|     |_______| /________| /________|" > /etc/ssh/sshd-banner
